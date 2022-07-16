a743211bb7b6c3c46c2626be0887c626ee94df67
xukejing <648460636@qq.com>
2022-07-17 00:10:15 +0800

    Create 1.sh

---
+++ libcamera-vid/1.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+source /etc/profile
+while true
+do
+sudo libcamera-vid -t 0 --inline -o - --width 1280 --height 720 --intra 25 \
+ --saturation 1.2 --bitrate 5000000 --profile high | \
+ffmpeg -re -stream_loop -1 -i "/home/test/out.aac" -f h264 \
+-i - -vcodec copy -acodec aac -b:a 128k -f flv \
+"rtmp://live-push.bilivideo.com/live-bvc/?streamname=\
+live_***************&\
+key=************************&schedule=rtmp&pflag=1"
+sleep 4
+done
