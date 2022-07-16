5ff2c705f3e79f2abad4f4a60c7ef251f2d2a56f
xukejing <648460636@qq.com>
2022-07-17 00:33:39 +0800

    Update 1.sh

--- libcamera-vid/1.sh
+++ libcamera-vid/1.sh
@@ -5,7 +5,7 @@ do
 sudo libcamera-vid -t 0 --inline -o - --width 1280 --height 720 --intra 25 \
  --saturation 1.2 --bitrate 5000000 --profile high | \
 ffmpeg -re -stream_loop -1 -i "/home/test/out.aac" -f h264 \
--i - -vcodec copy -acodec aac -b:a 128k -f flv \
+-i - -vcodec copy -acodec copy -f flv \
 "rtmp://live-push.bilivideo.com/live-bvc/?streamname=\
 live_***************&\
 key=************************&schedule=rtmp&pflag=1"
